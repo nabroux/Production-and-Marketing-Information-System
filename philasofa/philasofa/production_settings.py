927183466c4e417efaa6997d938a001f03ab767d
123nabroux <123nabroux@gmail.com>
2019-01-20 08:21:43 +0800

    django app

--- philasofa/philasofa/production_settings.py
+++ philasofa/philasofa/production_settings.py
@@ -6,6 +6,10 @@ DATABASES = {
     'default': dj_database_url.config(),
 }
 
+
+# Static asset configuration.
+STATIC_ROOT = 'staticfiles'
+
 # Honor the 'X-Forwarded-Proto' header for request.is_secure().
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
